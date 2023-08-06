24e0178b2b3bdc87d22604eb6d976593cca53381
Yura <Yura_sashin@mail.ru>
2023-08-06 17:02:28 +0300

    Решил задачу семинар6

--- seminar6/task6_1.py
+++ seminar6/task6_1.py
@@ -4,11 +4,13 @@ from seminar6.portfolio import calculate_portfolio_return, calculate_portfolio_v
 
 my_dick1 = {"AAPL": 10, "GOOGL": 5, "MSFT": 8}
 my_dick2 = {"AAPL": 155.25, "GOOGL": 2600.75, "MSFT": 800.50}
+a = 10000
+b = 15000
 
 summa = calculate_portfolio_value(my_dick1, my_dick2)
 print(summa)
 
-progress_ = calculate_portfolio_return(my_dick1,my_dick2)
+progress_ = calculate_portfolio_return(a,b)
 print(progress_)
 
 max_ = get_most_profitable_stock(my_dick1,my_dick2)
